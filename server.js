a7d3b2052c0d28dfd4b9c5a6028613696cc0c415
amrfahmy5 <45489219+amrfahmy5@users.noreply.github.com>
2022-05-07 04:27:23 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,78 @@
+const { config } = require("dotenv");
+
+const express = require("express");
+const socketIO = require("./services/socket.io");
+const app = express();
+const { static } = require("express");
+
+
+const helmet = require("helmet"); //sercure for website header
+const compression = require("compression"); //decreasing the amount of downloadable data
+const morgan = require("morgan");// all request logs show in terminal 
+const expressYouch = require("express-youch"); //presenting errors in a developer-friendly way
+
+
+const apiRoutes = require("./routes/api");
+const webRoutes = require("./routes/web");
+
+const bodyParser = require("body-parser");
+
+
+const sess = require("express-session");
+config({ cache: process.env["NODE_ENV"] === "production" });
+
+
+const edge = require("express-edge");
+const edgeService = require("./services/edge");
+edgeService(require("edge.js")).init();
+edge.config({ cache: true });
+
+
+
+const sessionMd = sess({
+  secret: process.env["secret"],
+  resave: false,
+  saveUninitialized: false,
+});
+
+// middlewares
+app.use(morgan("dev"));
+if(process.env.NODE_ENV === "production"){
+  app.use(compression())
+}
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(sessionMd);
+
+app.use(static("public"));
+app.use(edge.engine);
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "edge");
+
+// routes
+app.use(apiRoutes.router);
+app.use(webRoutes.router);
+// end routers
+
+
+if (process.env.NODE_ENV === "development") {
+  app.use(expressYouch.errorReporter());
+}
+
+
+const server = app.listen(process.env["port"], process.env["ip"], () =>
+  console.log(`server connected at port ${process.env["port"]} - ip ${process.env["ip"]}`)
+);
+
+socketIO.initialize(server)
+
+process.on("uncaughtException", (err) => {
+  console.error(err.stack);
+  process.exit(1);
+});
